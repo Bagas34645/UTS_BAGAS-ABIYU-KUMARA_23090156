5996168bdd93072c5d59327506e80afe61c16591
Bagas Abiyu Kumara <149683919+Bagas34645@users.noreply.github.com>
2024-05-07 17:07:44 +0700

    Update Soal_4.py

--- UTS_BagasAbiyuKumara_23090156/Soal_4.py
+++ UTS_BagasAbiyuKumara_23090156/Soal_4.py
@@ -5,15 +5,6 @@ tinggi_badan = float(input("Masukkan tinggi badan (m): "))
 
 bmi = modul.hitung_bmi(berat_badan, tinggi_badan)
 
-print("Berat Badan Anda adalah:", berat_badan, "Kg")
-print("Tinggi Badan Anda adalah:", tinggi_badan, "M")
-print("BMI Anda adalah:", bmi)
+modul.tampilkan_info(berat_badan, tinggi_badan, bmi)
 
-if bmi < 18.5:
-    print("Berat badan kurang")
-elif 18.5 <= bmi < 24.9:
-    print("Berat badan normal")
-elif 25 <= bmi < 29.9:
-    print("Kelebihan berat badan")
-else:
-    print("Obesitas")
+modul.keterangan_bmi(bmi)
